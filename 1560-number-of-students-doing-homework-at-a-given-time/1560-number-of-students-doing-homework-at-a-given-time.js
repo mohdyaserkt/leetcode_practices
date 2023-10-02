755720f90508481ede67b12f4d22063d86ab139b
Mohammed Yassar <yaserkt786@gmail.com>
2023-10-02 21:24:15 +0530

    Time: 46 ms (83.91%), Space: 41.3 MB (95.22%) - LeetHub

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    
+
+
+
+
+
+    let count=0
+    for(i=0;i<startTime.length;i++){
+        if(endTime[i]>=queryTime&&startTime[i]<=queryTime){
+            count++
+        }
+    }
+
+    return count
+};

6d46d01ad4acfd868c9d819844f8308bb51866ac
Mohammed Yassar <yaserkt786@gmail.com>
2023-12-05 11:08:49 +0530

    Time: 69 ms (9.87%) | Memory: 42.5 MB (44.08%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let num1=0
+    let num2=0
+
+
+    for(i=1;i<=n;i++){
+        if(i%m==0){
+num2+=i
+        }else{
+num1+=i
+        }
+    }
+
+    return num1-num2
+};
